db7b913e15cd9f7fefa57f254a6a84ebdd899623
appboypov <info@codaveto.com>
2026-04-17 12:49:04 +0200

    fix: finetuning models

--- turbo_promptable/lib/workspace/models/root/tool.dart
+++ turbo_promptable/lib/workspace/models/root/tool.dart
@@ -1,4 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:turbo_promptable/workspace/models/root/tool_set.dart';
 import 'package:turbo_promptable/workspace/models/tools/tool_ability.dart';
 
 export 'package:turbo_promptable/workspace/models/meta/t_promptable.dart';
@@ -19,4 +20,18 @@ class Tool extends TPromptable {
   factory Tool.fromJson(Map<String, dynamic> json) => _$ToolFromJson(json);
   @override
   Map<String, dynamic> toJson() => _$ToolToJson(this);
+
+  ToolSet asToolSet({
+    String? name,
+    String? description,
+    List<ToolAbility> Function(List<ToolAbility> abilities)? abilities,
+  }) {
+    return ToolSet(
+      description: description ?? this.description,
+      name: name ?? this.name,
+      abilities: abilities != null && this.abilities != null
+          ? abilities(this.abilities!)
+          : this.abilities,
+    );
+  }
 }
---
+++ turbo_promptable/lib/workspace/models/root/tool_set.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:turbo_promptable/workspace/models/root/tool.dart';
+import 'package:turbo_promptable/workspace/models/tools/tool_ability.dart';
+
+export 'package:turbo_promptable/workspace/models/meta/t_promptable.dart';
+export 'package:turbo_promptable/workspace/models/tools/tool_ability.dart';
+
+part 'tool_set.g.dart';
+
+@JsonSerializable(includeIfNull: false, explicitToJson: true)
+class ToolSet extends Tool {
+  const ToolSet({
+    required super.name,
+    required super.description,
+    super.abilities,
+  });
+
+  factory ToolSet.fromJson(Map<String, dynamic> json) => _$ToolSetFromJson(json);
+  @override
+  Map<String, dynamic> toJson() => _$ToolSetToJson(this);
+}
