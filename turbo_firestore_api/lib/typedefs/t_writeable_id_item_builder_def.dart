da82c5b3ceead80ef18b5d8358f25d1d4f0e3483
appboypov <info@codaveto.com>
2026-04-28 12:09:03 +0200

    feat: fix model enforcement

--- turbo_firestore_api/lib/typedefs/t_writeable_id_item_builder_def.dart
+++ turbo_firestore_api/lib/typedefs/t_writeable_id_item_builder_def.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:turbo_firestore_api/abstracts/t_model.dart';
 import 'package:turbo_serializable/abstracts/t_writeable_id.dart';
 
-typedef TWriteableIdItemBuilderDef<DTO extends TWriteableId, MODEL extends TModel<DTO>> =
+typedef TWriteableIdItemBuilderDef<DTO> =
     Widget Function(
       BuildContext context,
       DTO item,
